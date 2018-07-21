389e1375a02de2bee7a09413bf5850bc1a5f78f0
rehman <rehmanmomin@gmail.com>
2018-07-21 14:37:48 +0530

    without content

--- AggregateGDPPopulation/AggregateGDPPopulation.cs
+++ AggregateGDPPopulation/AggregateGDPPopulation.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace AggregateGDPPopulation
 {
     public class Class1
     {
+
+        public static void getAggregate()
+        {
+            string text;
+            using (var streamReader = new StreamReader(@"", Encoding.UTF8))
+            {
+                text = streamReader.ReadToEnd();
+            }
+        }
     }
 }
