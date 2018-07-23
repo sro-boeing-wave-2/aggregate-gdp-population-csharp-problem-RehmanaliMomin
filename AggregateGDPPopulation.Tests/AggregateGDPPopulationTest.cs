25df089d4060dea12954dddc4b197112fbf490b8
rehman <rehmanmomin@gmail.com>
2018-07-23 18:32:53 +0530

    sync program done

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -1,13 +1,24 @@
+using Newtonsoft.Json.Linq;
 using System;
+using System.IO;
 using Xunit;
+using AggregateGDPPopulation;
 
 namespace AggregateGDPPopulation.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Test1()             //..\..\..\..\AggregateGDPPopulation\data\datafile.csv
         {
+            Class1.Solution();
+            var actual = File.ReadAllText(@"..\..\..\expected-output.json");
+            var expected = File.ReadAllText(@"..\..\..\..\output\output.json");
+
+            JObject actualJson = JObject.Parse(actual);
+            JObject expectedJson = JObject.Parse(expected);
+
+            Assert.Equal(actualJson, expectedJson);
 
         }
     }
--- AggregateGDPPopulation/AggregateGDPPopulation.cs
+++ AggregateGDPPopulation/AggregateGDPPopulation.cs
@@ -1,19 +1,68 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AggregateGDPPopulation
 {
     public class Class1
     {
 
-        public static void getAggregate()
+        public static void Solution()
         {
-            string text;
-            using (var streamReader = new StreamReader(@"", Encoding.UTF8))
+
+            string data = File.ReadAllText(@"..\..\..\..\AggregateGDPPopulation\data\datafile.csv", Encoding.UTF8);
+            string dataWithoutQuotes = data.Replace("\"", "");
+            string[] rows = Regex.Split(dataWithoutQuotes, "\r\n");
+            string[] header = rows[0].Split(',');
+
+            string dataMapping = File.ReadAllText(@"..\..\..\..\AggregateGDPPopulation\data\continent-country.json", Encoding.UTF8);
+            var countryContinentMap = JsonConvert.DeserializeObject<Dictionary<string, string>>(dataMapping);
+
+            int countryNameIndex = Array.IndexOf(header, "Country Name");
+            int populationIndex = Array.IndexOf(header, "Population (Millions) 2012");
+            int gdpIndex = Array.IndexOf(header, "GDP Billions (USD) 2012");
+
+            Dictionary<string, OutputObject> outputMap = new Dictionary<string, OutputObject>();
+
+            for (int i = 1; i < rows.Length-2; i++)
             {
-                text = streamReader.ReadToEnd();
+                string[] row = rows[i].Split(',');
+                string country = row[countryNameIndex];
+                string continent = countryContinentMap[country];
+
+                if (!outputMap.ContainsKey(continent))
+                {
+                    OutputObject obj = new OutputObject();
+
+                    obj.GDP_2012 = float.Parse(row[gdpIndex]);
+                    obj.POPULATION_2012 = float.Parse(row[populationIndex]);
+                    outputMap.Add(continent, obj);
+                }
+                else
+                {
+
+                    OutputObject obj = outputMap[continent];
+                    obj.GDP_2012 += float.Parse(row[gdpIndex]);
+                    obj.POPULATION_2012 += float.Parse(row[populationIndex]);
+                }
             }
+
+            string outputString = JsonConvert.SerializeObject(outputMap);
+
+          
+            System.IO.File.WriteAllText(@"E:\workspace\Assignment-C#\aggregate-gdp-population-csharp-problem-RehmanaliMomin\output\output.json", outputString);
+
+        
+        }
+
+        class OutputObject
+        {
+            public float GDP_2012 { get; set; }
+            public float POPULATION_2012 { get; set; }
         }
     }
 }
